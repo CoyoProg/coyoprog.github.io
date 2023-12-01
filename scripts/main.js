0425c573056f99890c69413010174e1ab4c4605b
CoyoProg <corentin.gamedev@gmail.com>
2023-12-01 20:42:25 +0100

    Added nav

--- scripts/main.js
+++ scripts/main.js
@@ -3,31 +3,6 @@ let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
 /* Functions */
-function setUserName() 
-{
-    const myName = prompt("Please enter your name.");
-    if (!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem("name", myName);
-      myHeading.textContent = `Welcome, ${myName}`;
-    }
-  }
-
-/* Main */
-if (!localStorage.getItem("name")) 
-{
-    setUserName();
-} else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Welcome, ${storedName}`;
-}
-
-myButton.onclick = () => 
-{
-    setUserName();
-};
-
 myImage.onclick = () => 
 {
     const mySrc = myImage.getAttribute("src");
