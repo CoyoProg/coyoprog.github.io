3ad325ef8ae37d21d44db44e928c9e0267f06bf5
Paya Corentin <corentin.gamedev@gmail.com>
2023-12-27 13:47:55 +0100

    Update Plugins tab

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,41 @@ const myVideo = document.querySelector("video");
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
-/* Functions */
-myImage.onclick = () => 
-{
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/pusheen.png") {
-        myImage.setAttribute("src", "images/pusheenangry.png");
-    } else {
-        myImage.setAttribute("src", "images/pusheen.png");
-    }
-};
 
-myVideo.onclick = () => 
+/* Functions */
+if (myImage) 
 {
-    myVideo.play();
+    myImage.onclick = () => 
+    {
+        const mySrc = myImage.getAttribute("src");
+        if (mySrc === "images/pusheen.png") 
+        {
+            myImage.setAttribute("src", "images/pusheenangry.png");
+        } else 
+        {
+            myImage.setAttribute("src", "images/pusheen.png");
+        }
+    };
+}
+
+if (myVideo) 
+{
+    myVideo.onclick = () => 
+    {
+        myVideo.play();
+    };
+}
+
+const cardsElement = document.getElementById("cards");
+if (cardsElement) {
+    cardsElement.onmousemove = (e) => {
+        for (const card of document.getElementsByClassName("card")) {
+            const rect = card.getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
+
+            card.style.setProperty("--mouse-x", `${x}px`);
+            card.style.setProperty("--mouse-y", `${y}px`);
+        }
+    };
 }
